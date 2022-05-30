4da3b353fb246d9e4b5c2f57430c038478311665
White Nguyen <whitenguyen0909@gmail.com>
2022-05-30 12:12:47 -0600

    added images, css files, organizing layouts

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Header from "./Header";
-import Home from "../Home/Home";
+import Header from "../Others/Header";
+import Home from "../Others/Home";
 import About from "../Others/About";
 import Portfolio from "../Others/Portfolio";
 import Experience from "../Others/Experience";
