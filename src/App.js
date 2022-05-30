dd5ead4fc60851ba4bec410d81b6f9a8b50bfd68
White Nguyen <whitenguyen0909@gmail.com>
2022-05-29 20:29:42 -0600

    added react router dom to the dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Routes>
+
+      </Routes>
     </div>
   );
 }
